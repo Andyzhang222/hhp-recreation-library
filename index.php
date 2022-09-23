c6dae440eea6c3e023234c88c4637cedd177d8ef
Phuong Nguyen <ph450445@dal.ca>
2022-09-23 11:33:08 -0300

    modified index

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
-<html>
-    <body>
+<html lang="en">
+<head>
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
+</head>
+<body>
 
-        <h1>HHP Recreation Library</h1>
-        <p>Anything really</p>
+<h1>HHP Recreation Library</h1>
+<p>Anything really.</p>
 
-    </body>
-</html>
+</body>
+</html> 
