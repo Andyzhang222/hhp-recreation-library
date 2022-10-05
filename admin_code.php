20a380e08a8d26cbe48e49ef53899c6e0732df59
Effat Fariha <effatfariha@TE67D.WPA.Dal.Ca>
2022-10-05 12:22:54 -0300

    Added admin page

---
+++ admin_code.php
@@ -0,0 +1,41 @@
+ <?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sand";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$Admin_Code = $_POST['id'];
+		
+
+if (!($Admin_Code==""))
+{
+
+$sql="SELECT * FROM admin WHERE Id = '$_POST[id]' 	 ";
+$result = mysqli_query($conn, $sql);
+
+$row = mysqli_fetch_assoc($result);
+
+
+if($row["Id"]==$Admin_Code )
+   {
+	header("location: home.html");
+	}
+else
+    echo"Sorry, your credentials are not valid, Please try again.";
+
+}
+
+else 
+{
+	echo"Sorry, your credentials are not valid, Please try again.";
+}	
+
+
+?>
