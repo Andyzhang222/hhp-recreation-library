47a8be6f53dcde0dfdf12a775825a05add180cff
Blake Chasse <bl969679@dal.ca>
2022-10-07 12:33:52 -0300

    Upload New File

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer id="pg-footer">
+		<p>&copy; 2022. Dalhousie.</p>
+		<address id="contact-address">
+			123 Hacker Ave, Halifax, NS, Canada<br>
+			<a href="mailto:help@fakeemail.com">Reach out if you have any questions!</a>
+		</address>
+		<nav id="footer-nav">
+			<a href="#">Privacy Policy</a>
+			<a href="#">Terms of Use</a>
+			<a href="mailto:help@fakeemail.com">Contact Us</a>
+		</nav>
+	</footer>
+</body>
