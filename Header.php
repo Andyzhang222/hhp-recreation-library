309486fbae6034701cacb8340c024d1ba58e191e
Blake Chasse <bl969679@dal.ca>
2022-10-07 04:41:18 -0300

    Upload New File

---
+++ Header.php
@@ -0,0 +1,21 @@
+<head>
+    <title>HHP Recreation Library</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body class="grey lighten-2">
+<header id="pg-banner">
+		<h1>HHP Recreation Library</h1>
+
+		<nav id="primary-nav">
+			<a href="#">Home</a>
+			<a href="#">Link2</a>
+			<a href="#">Link3</a>
+			<a href="#">Link4</a>
+		</nav>
+        <div class="search-icon">
+            <form>
+                <input type="text" name="text" class="search-box">
+                <input type="submit" name="search" class = "search">
+            </form>
+	</header>
