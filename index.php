420f2ac327c03e44e1669d60a0b1cf1c0e7da621
blake <blakechasse27@gmail.com>
2022-10-14 11:52:42 -0300

    modified header, footer and home page

--- index.php
+++ index.php
@@ -5,8 +5,39 @@
     CloseConn($conn);
 ?>
 
-<h1>HHP Recreation Library</h1>
-<p>Anything really.</p>
+<section id="item-display">
+	<h2>All Available Items</h2>
+		<section id="display-item1" class="art-summary">
+			<h3>ITEM 1</h3>
+			<p>Short description of the product and details about the product such as it's primary uses</p>
+			<p><a href="articles/main-reactor.html">Read more &raquo;</a></p>
+		</section>
+		<section id="display-item2" class="art-summary">
+			<h3>ITEM 2</h3>
+			<p>Short description of the product and details about the product such as it's primary uses.</p>
+			<p><a href="articles/bad-motivator.html">Read more &raquo;</a></p>
+		</section>
+		<section id="display-item3" class="art-summary">
+			<h3>ITEM 3</h3>
+			<p>Short description of the product and details about the product such as it's primary uses.</p>
+			<p><a href="articles/wookies.html">Read more &raquo;</a></p>
+		</section>
+		<section id="display-item4" class="art-summary">
+			<h3>ITEM 4</h3>
+			<p>Short description of the product and details about the product such as it's primary uses.</p>
+			<p><a href="articles/human-cyborg-comm.html">Read more &raquo;</a></p>
+		</section>
+        <section id="display-item5" class="art-summary">
+			<h3>ITEM 5</h3>
+			<p>Short description of the product and details about the product such as it's primary uses.</p>
+			<p><a href="articles/human-cyborg-comm.html">Read more &raquo;</a></p>
+		</section>
+        <section id="display-item6" class="art-summary">
+			<h3>ITEM 6</h3>
+			<p>Short description of the product and details about the product such as it's primary uses.</p>
+			<p><a href="articles/human-cyborg-comm.html">Read more &raquo;</a></p>
+		</section>
+	</section>
 
 <?php
     require "includes/footer.php"; 
