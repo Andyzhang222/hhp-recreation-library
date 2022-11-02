9b3f38947c094f07c6be85c5be257a04f1300ebd
blake <blakechasse27@gmail.com>
2022-11-02 12:20:57 -0300

    initial commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,10 @@
-<!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-<div class="container">
-  <footer class="py-3 my-4">
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-    </ul>
-    <p class="text-center text-muted">&copy; 2022 Company, Inc</p>
+<div id="div-footer">
+  <footer class="border-top">
+    <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"></a>
+    <span class="mb-3 mb-md-0 text-muted">&copy;Dalhousie Faculty of Health and Human Performance</span>
   </footer>
+</div>
+
 </div>
 </body>
 
