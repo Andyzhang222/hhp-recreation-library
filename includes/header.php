6e17b1cf76c1f80e3d90c6fced58d8013512486b
blake <blakechasse27@gmail.com>
2022-11-15 22:35:49 -0400

    completed search bar

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,6 @@
 <?php
-if($_GET["q"] == "Search..."){
-    header("Location: index.php");
-}
-if($_GET["q"] !== ""){
-    require "includes/db-connection.php";
+if($_POST["q"] !== "null"){
+    require "db-connection.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,35 +8,6 @@ if($_GET["q"] !== ""){
         <title>HHP Recreation Library</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-            body {
-                font-family: Arial, Helvetica, sans-serif;
-            }
-            nav  {
-                margin-right: 2%;
-            }
-            #searchBar {
-                border: 2px solid #000000;
-                border-left: none;
-                font-size: 16px;
-                padding: 10px;
-                outline: none;
-                width: 250px;
-                float: right;
-            }
-            #searchBtn {
-                border: 2px solid #000000;
-                font-size: 16px;
-                padding: 10px;
-                background: #f1d829;
-                font-weight: bold;
-                cursor: pointer;
-                outline: none;
-                float: right;
-
-            }
-            
-        </style>
 
         <link rel="stylesheet" href="../css/styles.css">
 
@@ -60,29 +28,13 @@ if($_GET["q"] !== ""){
                         <a class="me-3 py-2 text-dark text-decoration-none" href="http://localhost:8888/">Equipment</a>
                         <a class="py-2 text-dark text-decoration-none" href="http://localhost:8888/pages/admin-login.php">Admin</a>
                     </nav>
-                    <form action="index.php" method="GET" id="searchForm">
+                    <form action="../pages/search.php" method="POST" id="searchForm">
                         <input type="text" name="q" id="searchBar" placeholder="Search..." maxlength="30" autocomplete="on"/>
-                        <input type="submit" id="searchBtn" value="Search"/>
+                        <button type="submit" name="submit-search" id="searchBtn">Search</button>
                     </form>
-                    <?php
-                    if(!isset($q)){
-                        echo " ";
-                    } else {
-                    $searchQuery = "SELECT * FROM `equipment_type` WHERE description LIKE '%$q%';";
-                    $searchResult = $conn->query($searchQuery);
-                    
-                    if ($searchResult->num_rows == 0) {
-                      echo "<p>No item returned.</p>";
-                    } else {
-                      while ($result = $searchResult->fetch_assoc()) {
-                        $description
-                      }
-                    }
-
-                    ?> 
             </header>
-<?php
+<?php                
 } else {
     header("Location: index.php");
-}
+  }
 ?>
