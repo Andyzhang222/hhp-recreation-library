8d835a75b0113d296d41a6e2927e4bea9a97e59f
Phuong Nguyen <ph450445@dal.ca>
2022-12-03 03:46:44 -0400

    update UI

--- pages/update.php
+++ pages/update.php
@@ -1,99 +1,62 @@
 <?php
-    session_start();
+	session_start();
 
-    if (!isset($_SESSION['admin'])) {
+	if (!isset($_SESSION['admin'])) {
         header("Location: ../index.php?no-access=1");
         exit();
     }
 
-    require_once "../includes/db-connection.php";
-    require "../includes/header.php";
+    require '../includes/header.php';
+    require_once '../includes/db-connection.php';
+
+	$message = "";
+	if (isset($_POST['update-button'])) {
+        $newQuant = htmlspecialchars(stripslashes(trim($_POST['update-quant'])));
+		$itemID = $_POST['id'];
+		$updateQuery = "UPDATE `equipment_type` SET quantity='$newQuant' WHERE id='$itemID';";
+		$updateResult = $conn->query($updateQuery);
+
+		if (!$updateResult) {
+			$message = "<p class='alert alert-success text-center'>Failed to update item quantity.</p>";
+		} else {
+			$message = "<p class='alert alert-success text-center'>Item updated.</p>";
+		}
+	}
 ?>
+<div class="request-info">
+	<div class="my-3 p-3 bg-body rounded shadow-sm">
+		<?php echo $message; ?>
+		<h6 class="border-bottom pb-2 mb-0">Update equipment quantity</h6>
+		<?php
+			$itemQuery = "SELECT * FROM `equipment_type`;";
+			$itemResult = $conn->query($itemQuery);
+			while ($row = $itemResult->fetch_assoc()) {
+				$description = $row['description'];
+				$item_id = $row['id'];
+				$item_code = $row['code'];
+				$imageSrc = "../img/item-images/" . $item_code . ".png";
+		?>
+		<div class="d-flex text-muted pt-3">
+		    <img src="<?php echo $imageSrc; ?>" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"/>
 
-<main>
-    <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-        <h1 class="display-4 fw-normal">Update Inventory</h1>
-      
-    <div class="album py-5">
-        <div class="container">
-            <form method = "post" action="../includes/update-quantity.php" >
-                <?php
-                    $catCountQuery = "SELECT COUNT(*) FROM `equipment_category`;";
-                    $catCountResult = $conn->query($catCountQuery);
-                    
-                    if ($catCountResult->num_rows == 0) {
-                        echo "<p class='text-dark'>No category found.</p>";
-                    } else {
-                        $fetchResult = $catCountResult->fetch_assoc();
-                        $numCat = (int) $fetchResult['COUNT(*)'];
-
-                        for ($i=1; $i <= $numCat; $i++) {
-                            $retrieveCatQuery = "SELECT * 
-                            FROM `equipment_category` 
-                            WHERE id = $i;";
-
-                            $retrieveCatResult = $conn->query($retrieveCatQuery);
-                            if ($retrieveCatResult->num_rows == 0) {
-                                echo "<p class='text-dark'>No category with id " . $i . " exist.</p>";
-                            } else {
-                                $queryResult = $retrieveCatResult->fetch_assoc();
-                                $category = $queryResult['description'];
-                            }
-                            ?>
-                            <h2 class="category"><?php echo $category; ?></h2>
-                            <div class="g-3 p-3">
-                            <?php
-                                $catQuery = "SELECT * 
-                                FROM `equipment_type` 
-                                WHERE category = $i;";
-                                $catResult = $conn->query($catQuery);
-
-                            if ($catResult->num_rows == 0) {
-                                echo "<p class='text-dark'>No items available for borrow in this category.</p>";
-                            } else {
-                                $k = 1;
-                                while ($result = $catResult->fetch_assoc()) {
-                                    $description = $result['description'];
-                                    $item_id = $result['id'];
-                                    $code = $result['code'];
-                                    $quantity = $result['quantity'];
-                                    ?>
-                            <div class="col">
-                                <div class="card shadow-sm">
-                            
-                                <div class="card-body">
-                                <H3 class="fw-light"><?php echo $description?></H3>
-                                </div>
-                            </div>
-
-                            <div class="card-body2">
-                                <p>Enter new item quantity</p>
-                                <input type="text" id = "<?php echo $item_id?>" class= "Input" name = "<?php echo $item_id?>" value = "<?php echo $quantity?>">
-                            </div> 
-
-                            </div>
-                                <?php
-                                    $k++;
-                                }
-                            }
-                            ?>
-                </div>
-                <?php
-                        }
-                    }
-                ?>
-                <!-- button here -->
-                <input type="submit" name="Update" class="submit1" value="Submit">
-            </form>
-        </div>
-    </div>
-</main>
-
-
-
-
+            <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
+                <form method="post" action="update.php">
+                    <div class="d-flex justify-content-between">
+                        <strong class="text-dark"><?php echo $description; ?></strong>
+                        <input type="hidden" name="id" value="<?php echo $item_id; ?>" />
+                        <input class="link-primary remove-button" type="submit" name="update-button" value="Update quantity" />
+                    </div>
+                    <span class="d-block">
+                        <label for="update-quant">Quantity:</label>
+                        <textarea id="update-quant" name="update-quant"><?php echo $row['quantity']; ?></textarea>
+                    </span>
+                </form>
+		    </div>
+	    </div>
+		<?php } ?>
+	</div>
+</div>
 
 <?php
-  
-    require "../includes/footer.php";
+	require "../includes/footer.php";
 ?>
