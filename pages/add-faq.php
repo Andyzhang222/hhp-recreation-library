289f8eba82417adc39053ae02bd7fffc6cd372c6
Phuong Nguyen <ph450445@dal.ca>
2022-12-05 03:00:20 -0400

    added faq function

---
+++ pages/add-faq.php
@@ -0,0 +1,53 @@
+<?php
+  session_start();
+
+  if (!isset($_SESSION['admin'])) {
+    header("Location: ../index.php?no-access=1");
+    exit();
+  }
+
+  require '../includes/header.php';
+  require '../includes/db-connection.php';
+
+  $message = "";
+
+  if (isset($_POST['submit-faq'])) {
+    $question = htmlspecialchars(stripslashes(trim($_POST['question'])));
+    $answer = htmlspecialchars(stripslashes(trim($_POST['answer'])));
+
+    $insertQuery = "INSERT INTO `faq` VALUES(NULL, '$question', '$answer');";
+    $insertResult = $conn->query($insertQuery);
+
+    if ($insertResult) {
+      $message = "<p class='alert alert-success text-center'>FAQ added to the database.</p>";
+    } else {
+      $message = "<p class='alert alert-danger text-center'>Failed to add FAQ.</p>";
+    }
+  }
+?>
+
+<main class="request-info">
+  <div class="py-2 text-center">
+    <?php echo $message; ?>
+    <h2>Add new FAQ</h2>
+  </div>
+  <form class="needs-validation" method="post" action="add-faq.php">
+    <div class="row g-3">
+        <div class="col-12">
+            <label for="question" class="form-label">Enter the question here:</label>
+            <input type="text" class="form-control" id="question" name="question" required>
+        </div>
+
+        <div class="col-12">
+            <label for="answer" class="form-label">Enter the answer here:</label>
+            <textarea class="form-control" id="answer" name="answer" rows="8" required></textarea>
+        </div>
+    </div>
+
+    <button class="w-100 btn btn-warning btn-lg mt-4" type="submit" name="submit-faq">Add FAQ</button>
+  </form>
+</main>
+
+<?php
+  require "../includes/footer.php";
+?>
--- pages/admin-page.php
+++ pages/admin-page.php
@@ -12,18 +12,18 @@
 
 <div id="admin-portal">
     <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-        <h1 class="display-4 fw-normal">Administrator Portal</h1>
+        <h1 class="h2 fw-normal">Administrator Portal</h1>
     </div>
 
-    <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+    <div class="row mb-3 text-center">
         <div class="col">
             <div class="card mb-4 rounded-3 shadow-sm">
             <div class="card-header py-3">
                 <h4 class="my-0 fw-normal">Manage inventory</h4>
             </div>
             <div class="card-body">
-                <a href="add-item.php" id="add-item" class="btn-spacing w-100 btn btn-lg btn btn-outline-dark">Add new item</a>
-                <a href="update.php" id ="update-quantity" class="btn-spacing w-100 btn btn-lg btn btn-outline-dark">Update item quantity</a>
+                <a href="add-item.php" id="add-item" class="btn-spacing w-100 btn btn-lg btn-outline-dark">Add new item</a>
+                <a href="update.php" id ="update-quantity" class="btn-spacing w-100 btn btn-lg btn-outline-dark">Update item quantity</a>
                 <a href="remove-item.php" id="remove-item" class="btn-spacing w-100 btn btn-lg btn-outline-dark">Remove item</a>
             </div>
             </div>
@@ -44,11 +44,23 @@
         <div class="col">
             <div class="card mb-4 rounded-3 shadow-sm">
             <div class="card-header py-3">
-                <h4 class="my-0 fw-normal">Manage admin access</h4>
+                <h4 class="my-0 fw-normal">Manage access</h4>
             </div>
             <div class="card-body">
                 <a href="access-codes.php" id="access-code" class="btn-spacing w-100 btn btn-lg btn-outline-dark">Current access codes</a>
                 <a href="add-code.php" id="add-code" class="btn-spacing w-100 btn btn-lg btn-outline-dark">Add new code</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+                <h4 class="my-0 fw-normal">Manage FAQ</h4>
+            </div>
+            <div class="card-body">
+                <a href="add-faq.php" id="access-code" class="btn-spacing w-100 btn btn-lg btn-outline-dark">Add new FAQ</a>
+                <a href="current-faq.php" id="add-code" class="btn-spacing w-100 btn btn-lg btn-outline-dark">Current FAQ</a>
             </div>
             </div>
         </div>
