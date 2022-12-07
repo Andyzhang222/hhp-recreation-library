99b2c2af1faf54d83a1e97ca57d2773e6634cf95
Phuong Nguyen <ph450445@dal.ca>
2022-12-07 01:39:23 -0400

    update db

--- db/hhp_recreation_library.sql
+++ db/hhp_recreation_library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 06, 2022 at 05:41 PM
+-- Generation Time: Dec 07, 2022 at 05:39 AM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -39,7 +39,7 @@ CREATE TABLE `admin_code` (
 
 INSERT INTO `admin_code` (`code`, `date_created`, `expire_date`) VALUES
 ('12abc34', '2022-12-04', '2022-12-30'),
-('2468abc', '2022-12-04', '2022-12-31');
+('2468abc', '2022-12-06', '2023-01-07');
 
 -- --------------------------------------------------------
 
@@ -97,7 +97,7 @@ INSERT INTO `equipment_type` (`id`, `code`, `category`, `description`, `quantity
 (11, 'PLAYING_CARD', 4, 'Playing cards', 4),
 (12, 'CRANIUM_CARD', 4, 'Cranium cards', 1),
 (13, 'CRIBBAGE', 4, 'Cribbage board', 1),
-(14, 'CROSSWORDS', 4, 'Crosswords books', 3),
+(14, 'CROSSWORDS', 4, 'Crosswords books', 2),
 (15, 'DICE', 4, 'Dice', 2),
 (16, 'UNO', 4, 'Uno Cards', 1),
 (17, 'REC_SHIRT', 5, 'Dalhousie Recreation T-Shirts', 2),
@@ -121,18 +121,19 @@ INSERT INTO `equipment_type` (`id`, `code`, `category`, `description`, `quantity
 (48, 'BOCCE', 11, 'Bocce ball sets', 2),
 (49, 'FOOTBALL', 11, 'Footballs', 1),
 (50, 'FRISBEE', 11, 'Frisbee', 4),
-(51, 'SOCCER', 11, 'Soccer Balls', 2),
+(51, 'SOCCER', 11, 'Soccer Balls', 3),
 (52, 'TENNIS_BALL', 11, 'Tennis Balls', 4),
 (53, 'GOGGLES', 1, 'Reduced visibility goggles', 1),
 (54, 'BLIND_FOLD', 1, 'Blindfolds', 2),
-(55, 'SHARPIE', 2, 'Sharpie Markers (set of 24)', 2),
+(55, 'SHARPIE', 2, 'Sharpie Markers (set of 24)', 3),
 (56, 'CONNECT_4', 4, 'Connect 4 Game', 1),
 (57, 'TILE_RUMMY', 4, 'Tile Rummy Game', 1),
 (58, 'JENGA', 4, 'Giant/yard Jenga game', 1),
 (59, 'BOAT_ROPE_2m', 3, 'Boat rope (2m)', 3),
 (62, 'TABLE_TENNIS', 11, 'Table tennis set', 2),
 (63, 'ALL_PURPOSE_BALL', 11, 'All purpose ball', 2),
-(64, 'VOLLEYBALL', 11, 'Volleyball', 2);
+(64, 'VOLLEYBALL', 11, 'Volleyball', 2),
+(66, 'PENCIL_COLOR', 2, 'Pencil Color', 3);
 
 -- --------------------------------------------------------
 
@@ -153,8 +154,25 @@ CREATE TABLE `faq` (
 INSERT INTO `faq` (`id`, `question`, `answer`) VALUES
 (1, 'Gummies cheesecake chocolate carrot cake I love tootsie roll tootsie roll?', 'Pie lemon drops icing candy canes jelly caramels chupa chups I love topping. Sweet roll brownie gummi bears oat cake liquorice. Pastry candy caramels jelly beans lemon drops. Gingerbread cake dragée chocolate cake bonbon. Bear claw bear claw sesame snaps I love tiramisu cake. Lemon drops chocolate gummies jelly icing. Icing biscuit halvah sweet cupcake chupa chups candy icing wafer.'),
 (2, 'Carrot cake lemon drops candy oat cake cupcake I love lollipop?', 'Marzipan candy canes jelly beans cotton candy liquorice I love I love bonbon. I love muffin bear claw oat cake soufflé. Powder sugar plum donut ice cream muffin carrot cake cake halvah sweet.'),
-(3, 'Wafer jelly-o chupa chups oat cake shortbread cheesecake?', 'Brownie liquorice I love biscuit jujubes cotton candy gummi bears. Ice cream toffee cotton candy soufflé bonbon. Chocolate cake pudding I love sweet roll bonbon croissant. Oat cake gummi bears cake I love candy canes chocolate croissant. Sesame snaps tootsie roll gingerbread I love powder chocolate cake danish dessert bear claw. I love chupa chups powder halvah chocolate bar danish. I love gummi bears macaroon gummies sweet chocolate cake. I love caramels pie I love candy sweet roll.'),
-(4, 'I love sweet cotton candy chupa chups carrot cake tootsie roll bear claw biscuit candy?', 'Marzipan biscuit lollipop gummi bears chocolate cake chupa chups sesame snaps. Cheesecake halvah bonbon icing I love jelly. Carrot cake topping chocolate cake I love bear claw.');
+(3, 'Wafer jelly-o chupa chups oat cake shortbread cheesecake?', 'Brownie liquorice I love biscuit jujubes cotton candy gummi bears. Ice cream toffee cotton candy soufflé bonbon. Chocolate cake pudding I love sweet roll bonbon croissant. Oat cake gummi bears cake I love candy canes chocolate croissant. Sesame snaps tootsie roll gingerbread I love powder chocolate cake danish dessert bear claw. I love chupa chups powder halvah chocolate bar danish. I love gummi bears macaroon gummies sweet chocolate cake. I love caramels pie I love candy sweet roll.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locker_code`
+--
+
+CREATE TABLE `locker_code` (
+  `code_combination` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `locker_code`
+--
+
+INSERT INTO `locker_code` (`code_combination`) VALUES
+(234),
+(123);
 
 -- --------------------------------------------------------
 
@@ -181,12 +199,15 @@ CREATE TABLE `order` (
 
 INSERT INTO `order` (`order_number`, `item_list`, `borrower_name`, `borrower_email`, `study_program`, `date_needed`, `purpose`, `return_date`, `order_processed`, `order_returned`) VALUES
 (4, '{\"4\": \"1\", \"21\": \"3\", \"44\": \"10\"}', 'phuong nguyen', 'you@dal.ca', 'Bachelor of Awesome', '2022-12-05', 'personal', '2022-12-29', 1, 1),
-(5, '{\"49\": \"3\", \"52\": \"4\"}', 'phuong nguyen', 'you@dal.ca', 'Bachelor of Awesome', '2022-12-06', 'I want it I got it', '2022-12-30', 0, 0),
+(5, '{\"49\": \"3\", \"52\": \"4\"}', 'phuong nguyen', 'you@dal.ca', 'Bachelor of Awesome', '2022-12-06', 'I want it I got it', '2022-12-30', 1, 0),
 (6, '{\"22\": \"1\", \"47\": \"2\", \"50\": \"1\"}', 'lauren', 'lauren@dal.ca', 'Bachelor of CompSci', '2022-12-07', 'in-class-use', '2022-12-22', 1, 0),
-(7, '{\"51\": \"1\", \"55\": \"1\"}', 'khaleda', 'khaleda@dal.ca', 'Bachelor of CompSci', '2022-12-06', 'extra-curr', '2022-12-14', 1, 0),
+(7, '{\"51\": \"1\", \"55\": \"1\"}', 'khaleda', 'khaleda@dal.ca', 'Bachelor of CompSci', '2022-12-06', 'extra-curr', '2022-12-14', 1, 1),
 (8, '{\"25\": \"3\", \"26\": \"2\"}', 'blake', 'blake@dal.ca', 'Bachelor of CompSci', '2022-12-07', 'For use outside the classroom (i.e., in community), to fulfill a course requirement', '2022-12-30', 0, 0),
 (9, '{\"11\": \"1\", \"24\": \"3\", \"42\": \"4\", \"50\": \"1\", \"59\": \"1\"}', 'haoran', 'haoran@dal.ca', 'Bachelor of CompSci', '2022-11-24', 'For use in a volunteer or work placement', '2022-11-30', 0, 0),
-(10, '{\"14\": \"1\", \"44\": \"5\", \"52\": \"2\"}', 'hillary', 'hillary@dal.ca', 'Bachelor of CompSci', '2022-12-06', 'For use with an extracurricular group on campus (i.e., SAHHPer)', '2022-12-30', 1, 0);
+(10, '{\"14\": \"1\", \"44\": \"5\", \"52\": \"2\"}', 'hillary', 'hillary@dal.ca', 'Bachelor of CompSci', '2022-12-06', 'For use with an extracurricular group on campus (i.e., SAHHPer)', '2022-12-30', 1, 0),
+(11, '{\"14\": \"2\"}', 'olive', 'olive@dal.ca', 'Bachelor of Applied CompSci', '2022-12-08', 'In-classroom use', '2022-12-24', 1, 1),
+(12, '{\"14\": \"3\", \"66\": \"2\"}', 'remi', 'remi@dal.ca', 'Bachelor of Arts', '2022-12-09', 'For use outside the classroom (i.e., in community), to fulfill a course requirement', '2022-12-15', 1, 1),
+(13, '{\"14\": \"3\"}', 'remi', 'remi@dal.ca', 'Bachelor of Arts', '2022-12-08', 'For use outside the classroom (i.e., in community), to fulfill a course requirement', '2022-12-29', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -208,7 +229,9 @@ CREATE TABLE `returns` (
 
 INSERT INTO `returns` (`return_id`, `order_number`, `notes`, `return_date`, `processed`) VALUES
 (1, 4, '2 balloons popped', '2022-12-03', 1),
-(3, 7, 'One soccer ball is deflated', '2022-12-03', 0);
+(3, 7, 'One soccer ball is deflated', '2022-12-03', 1),
+(4, 11, '1 book damaged', '2022-12-06', 1),
+(5, 12, '', '2022-12-06', 1);
 
 --
 -- Indexes for dumped tables
@@ -266,7 +289,7 @@ ALTER TABLE `equipment_category`
 -- AUTO_INCREMENT for table `equipment_type`
 --
 ALTER TABLE `equipment_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
 -- AUTO_INCREMENT for table `faq`
@@ -278,13 +301,13 @@ ALTER TABLE `faq`
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `order_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `order_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `returns`
 --
 ALTER TABLE `returns`
-  MODIFY `return_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `return_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
